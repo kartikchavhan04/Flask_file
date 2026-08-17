1a459935ce4ac27ae9511c1557f873f615c691b0
Kartik Chavhan <kartikpchavhan2004@gmail.com>
2026-08-17 23:44:54 +0530

    commating something

--- pg/pg/config/db_config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:Bharat123@localhost/mobile_shop"
-    )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SQLALCHEMY_ECHO = True
